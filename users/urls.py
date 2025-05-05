8476b925ad2f1a5fa788afbc206b961579fb2fd3
wyonyango <wycliffealphus7@gmail.com>
2025-05-05 09:43:30 +0300

    refactor:remove login pattern to avoid duplication since it has been done in the main app

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', v.register, name='register'),
     path('company/', v.CompanySignUpView.as_view(), name='register_company'),
     path('customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
-    path('login/', v.LoginUserView, name='login_user')
 ]
